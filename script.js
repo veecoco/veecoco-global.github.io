8ba08bb7e2e6da8425bf96ce36014f3fa9cf0e0c
veecoco <99670053+veecoco@users.noreply.github.com>
2022-02-14 16:22:10 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+/* DEFINE CURRENT VALUES */
+
+const LESSON_NUMBER = '700'
+const COURSE_NUMBER = '20'
+const MEALS_DONATED = '6.162'
+
+/* CHANGE LESSON NUMBER */
+
+const lessonNumber = document.querySelectorAll('.lesson_number')
+lessonNumber.forEach((lesson) => {
+  lesson.textContent = LESSON_NUMBER
+})
+
+/* CHANGE COURSE NUMBER */
+
+const courseNumber = document.querySelectorAll('.course_number')
+courseNumber.forEach((course) => {
+  course.textContent = COURSE_NUMBER
+})
+
+/* CHANGE MEALS DONATED NUMBER */
+
+const mealsDonated = document.querySelectorAll('.meals_donated')
+mealsDonated.forEach((course) => {
+  course.textContent = MEALS_DONATED
+})
